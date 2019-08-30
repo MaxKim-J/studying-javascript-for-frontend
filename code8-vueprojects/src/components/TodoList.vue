066f4d7e9aff7ea0b80d1d0772052a82a3973888
Max Jonghyuk Kim <hwaseen@gmail.com>
2019-08-31 01:44:44 +0900

    리스트 애니메이션 삽입

--- code8-vueprojects/src/components/TodoList.vue
+++ code8-vueprojects/src/components/TodoList.vue
@@ -1,60 +1,79 @@
 <template>
-    <section>
-        <ul>
-            <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem" class="shadow">
-            <i class="checkBtn fa fa-check" aria-hidden="true"></i>
-            <span>{{ todoItem }}</span>
-            <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
-                <i class="fas fa-trash-alt" aria-hidden="true"></i>
-            </span>
-            </li>
-        </ul>
-    </section>
+  <section>
+    <transition-group name="list" tag="ul">
+      <li v-for="(todoItem, index) in propsdata" v-bind:key="index" class="shadow">
+        <i class="checkBtn fa fa-check" aria-hidden="true"></i>
+        <span>{{ todoItem }}</span>
+        <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+          <i class="fas fa-trash-alt" aria-hidden="true"></i>
+        </span>
+      </li>
+    </transition-group>
+  </section>
 </template>
 
 <script>
 export default {
-    props:['propsdata'],
-    //created 라이프 사이클 훅
-    methods: {
-        removeTodo(todoItem, index){
-            this.$emit('removeTodo', todoItem, index)
-        }
+  props: ["propsdata"],
+  //created 라이프 사이클 훅
+  methods: {
+    removeTodo(todoItem, index) {
+      this.$emit("removeTodo", todoItem, index);
     }
-}
+  }
+};
 </script>
 
 <style scope>
+span {
+  margin: auto 0;
+}
 
-    span{
-        margin: auto 0;
-    }
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0px;
+  text-align: left;
+}
 
-    ul{
-        list-style-type: none;
-        padding-left: 0px;
-        margin-top: 0px;
-        text-align: left;
-    }
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  margin: 20px 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
 
-    li{
-        display:flex;
-        min-height: 50px;
-        height: 50px;
-        margin: 20px 0;
-        padding: 0 0.9rem;
-        background: white;
-        border-radius: 5px;
-    }
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
 
-    .checkBtn{
-        line-height: 45px;
-        color: #62acde;
-        margin-right: 5px;
-    }
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
 
-    .removeBtn{
-        margin-left: auto;
-        color: #de4343;
-    }
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+
+.list-move {
+  transition: transform 1s;
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s;
+}
+
+.list-enter,
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
 </style>

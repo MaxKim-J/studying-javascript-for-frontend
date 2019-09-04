cfeb24b9b2166fb3f34de4be7fe1bb864d63170c
Max Jonghyuk Kim <hwaseen@gmail.com>
2019-09-04 12:17:07 +0900

    cli project propsdata

--- code10-vue-cli/src/App.vue
+++ code10-vue-cli/src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!-- 최상위 인스턴스, 템플릿 루트는 하나만 있어야 함-->
+  <div>
+    <app-header v-bind:propsdata="str"></app-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      str: 'hi!!',
+    }
+  },
+  components:{
+    'app-header': AppHeader
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ code10-vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+      <h1>header</h1>
+      <div>
+          {{ propsdata }}
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    props: ['propsdata']
+}
+</script>
+
+<style>
+
+</style>

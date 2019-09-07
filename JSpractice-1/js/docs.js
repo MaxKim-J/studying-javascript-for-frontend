450e6ecbed09e44decf5af0bc56fe0fd4b14df9b
Max Jonghyuk Kim <hwaseen@gmail.com>
2019-09-07 16:42:22 +0900

    navbar, main section html/css complete

---
+++ JSpractice-1/js/docs.js
@@ -0,0 +1,13 @@
+//scroll event listner
+
+window.addEventListener('scroll', () => {
+    const scrolled = window.scrollY;
+    console.log(scrolled);
+});
+
+
+// main content fadeaway
+
+// navbar 뿅 pop out from out of the windows
+
+// navbar hovering event

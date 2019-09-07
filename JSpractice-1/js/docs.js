9f81afa1467dce61d57b29154ef6d72bb7cb90fa
Max Jonghyuk Kim <hwaseen@gmail.com>
2019-09-07 20:20:21 +0900

    main content fadeaway

--- JSpractice-1/js/docs.js
+++ JSpractice-1/js/docs.js
@@ -1,13 +1,28 @@
-//scroll event listner
+// scroll event listner
+// main content fadeaway
+// navbar "google 문서로 이동" pop out from out of the windows
 
 window.addEventListener('scroll', () => {
-    const scrolled = window.scrollY;
-    console.log(scrolled);
+    const scrolled = Math.floor(window.scrollY);
+
+    if (scrolled > 932) {
+        const navPop = document.getElementById('js-header-right-popout');
+        var fade = document.getElementById('js-section-main-fade')
+        
+        navPop.innerHTML = "Google 문서로 이동"
+        navPop.style.backgroundColor = "#4386FC"
+        navPop.style.color = "white"
+        navPop.style.fontWeight = "bold"
+
+    } else {
+        const navPop = document.getElementById('js-header-right-popout');
+        var fade = document.getElementById('js-section-main-fade')
+
+        fade.style.opacity = -scrolled / 932 + 1;
+
+        navPop.innerHTML = ""
+        navPop.style.backgroundColor = ""
+        navPop.style.color=""
+    }
 });
 
-
-// main content fadeaway
-
-// navbar 뿅 pop out from out of the windows
-
-// navbar hovering event

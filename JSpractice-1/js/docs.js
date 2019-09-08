efe60106d79b1e055c4c8b3d74ce526c0625ba0f
Max Jonghyuk Kim <hwaseen@gmail.com>
2019-09-08 22:38:46 +0900

    footer content input compleate

--- JSpractice-1/js/docs.js
+++ JSpractice-1/js/docs.js
@@ -1,23 +1,18 @@
 // scroll event listner
-// main content fadeaway
-// navbar "google 문서로 이동" pop out from out of the windows
+// main content fades away
+// navbar "google 문서로 이동" pops out from out of the windows
 
 window.addEventListener('scroll', () => {
     const scrolled = Math.floor(window.scrollY);
+    const navPop = document.getElementById('js-header-right-popout');
+    var fade = document.getElementById('js-section-main-fade')
 
     if (scrolled > 932) {
-        const navPop = document.getElementById('js-header-right-popout');
-        var fade = document.getElementById('js-section-main-fade')
-        
         navPop.innerHTML = "Google 문서로 이동"
         navPop.style.backgroundColor = "#4386FC"
         navPop.style.color = "white"
         navPop.style.fontWeight = "bold"
-
     } else {
-        const navPop = document.getElementById('js-header-right-popout');
-        var fade = document.getElementById('js-section-main-fade')
-
         fade.style.opacity = -scrolled / 932 + 1;
 
         navPop.innerHTML = ""
@@ -26,3 +21,6 @@ window.addEventListener('scroll', () => {
     }
 });
 
+
+// ajax, axios, es6 비동기처리
+// vue 문법, 
